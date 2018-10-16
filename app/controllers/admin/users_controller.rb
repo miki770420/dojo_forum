99ea766468d8c985d0820a357a5957cc150e5f33
Yun Ting Hung <s9923554@gmail.com>
2018-10-17 01:35:45 +0800

    setup admin root page with category and user list

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,11 +6,26 @@ class Admin::UsersController < ApplicationController
     @users = User.all
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      flash[:notice] = 'User is updated'
+    else
+      flash[:alert] = 'User fail to updated'
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
   private 
   def authenticate_admin
       unless current_user.admin?
         flash[:alert] = "Not allow!"
         redirect_to root_path
       end
+  end
+  
+  def user_params
+    params.require(:user).permit(:role)
   end
 end
