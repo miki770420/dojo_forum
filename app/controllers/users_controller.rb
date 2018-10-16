b73c56e2e38ab4625d2f9757a170421348979c0c
Yun Ting Hung <s9923554@gmail.com>
2018-10-16 18:40:10 +0800

    create user edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,35 @@
 class UsersController < ApplicationController
+  before_action :authenticate_owner, only: [:edit, :update]
   before_action :authenticate_user!
 
   def show
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "user was successfully updated"
+      redirect_to user_path(@user)
+    else
+      flash.now[:alert] = "user was failed to update"
+      render :edit
+    end
+  end
 
   private
   def user_params
     params.require(:user).permit(:name, :description, :avatar)
+  end
+
+  def authenticate_owner
+    @user = User.find(params[:id])
+    unless current_user == @user
+      flash[:alert] = "Not allow!"
+      redirect_to user_path(@user)
+    end
   end
 end
