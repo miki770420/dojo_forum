3037b29033da02a565b364669e95210e4235cb97
Yun Ting Hung <s9923554@gmail.com>
2018-10-21 21:02:31 +0800

    create friendship model and setup correlation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :replies, dependent: :destroy
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
 
   mount_uploader :avatar, AvatarUploader
 
