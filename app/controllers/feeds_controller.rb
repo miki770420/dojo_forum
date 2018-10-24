0a871ff8a126814fa9b875ffad6d6b588638391a
Yun Ting Hung <s9923554@gmail.com>
2018-10-24 22:48:47 +0800

    adjust format

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,6 +2,9 @@ class FeedsController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @user_count = User.all.count
+    @post_count = Post.all.where(:draft => false).count
+    @replies_count = Reply.all.count
     @popular_users = User.order(replies_count: :desc).limit(10)
     @popular_posts = Post.order(replies_count: :desc).limit(10)
   end
